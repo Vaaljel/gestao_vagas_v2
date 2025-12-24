ed963923380e0b92f98b5a195cb312179e4797e8
Vaaljel <vaaljoel108@gmail.com>
2025-12-24 21:51:09 +0000

    Configuração inicial: Spring Boot + Docker (PostgreSQL)

---
+++ src/main/java/pt/com/joelvaal/gestao_vagas_v2/GestaoVagasV2Application.java
@@ -0,0 +1,13 @@
+package pt.com.joelvaal.gestao_vagas_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoVagasV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoVagasV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pt/com/joelvaal/gestao_vagas_v2/GestaoVagasV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.com.joelvaal.gestao_vagas_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
